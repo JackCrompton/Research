0ce642a805338dc0381c33379cc2b3e4b7d10454
StraightOuttaCrompton <StraightOuttaCrompton@users.noreply.github.com>
2018-03-15 13:16:00 +0000

    Added sass example, media querries mixins not currently working

---
+++ SassExample/sass.js
@@ -0,0 +1,24 @@
+var sass = require('node-sass');
+var fs = require('fs');
+
+const input_scss = './index.scss';
+const output_scss = './dist.css';
+
+sass.render({
+    file: input_scss,
+    outFile: output_scss,
+    outputStyle: 'compressed'
+}, function (err, result) {
+    if (!err) {
+        // No errors during the compilation, write this result on the disk
+        fs.writeFile(output_scss, result.css, function (err) {
+            if (err) {
+                console.log('There was an error writing file to ' + output_scss);
+                console.log(err);
+            } else {
+                console.log(output_scss + ' was successfully saved');
+            }
+        });
+    }
+});
+
